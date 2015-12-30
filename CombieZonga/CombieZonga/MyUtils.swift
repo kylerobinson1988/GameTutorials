2e8304f447f85bef987dff21709b54266165452a
Sinkingstarship <kylerobinson1988@gmail.com>
2015-12-30 13:38:50 -0500

    Updates
    Lots of stuff for CombieZonga.

---
+++ CombieZonga/CombieZonga/MyUtils.swift
@@ -0,0 +1,137 @@
+//
+//  MyUtils.swift
+//  CombieZonga
+//
+//  Created by Kyle Brooks Robinson on 12/30/15.
+//  Copyright © 2015 Kyle Brooks Robinson. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    
+}
+
+func += (inout left: CGPoint, right: CGPoint) {
+    
+    left = left + right
+    
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    
+}
+
+func -= (inout left: CGPoint, right: CGPoint) {
+    
+    left = left - right
+    
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+    
+}
+
+func *= (inout left: CGPoint, right: CGPoint) {
+
+    left = left * right
+    
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+    
+}
+
+func *= (inout point: CGPoint, scalar: CGFloat) {
+    
+    point = point * scalar
+    
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+    
+}
+
+func /= (inout left: CGPoint, right: CGPoint) {
+    
+    left = left / right
+    
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+    
+}
+
+func /= (inout point: CGPoint, scalar: CGFloat) {
+    
+    point = point / scalar
+    
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func atan2(y: CGFloat, x: CGFloat) -> CGFloat {
+        return CGFloat(atan2f(Float(y), Float(x)))
+}
+
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrt(Float(a)))
+}
+
+#endif
+
+extension CGPoint {
+    
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y, x)
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
